2bb28767c63cf7eaa0e827b305726125ae1e4b1d
Abhinav Das <abhinav.adtechs@gmail.com>
2016-05-05 14:45:35 +0530

    Charts done.

--- app/src/main/java/abhinav/hackdev/co/amortizedanalysis/GPAFragment.java
+++ app/src/main/java/abhinav/hackdev/co/amortizedanalysis/GPAFragment.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class GPAFragment extends Fragment{
 
     private TextView textView ;
--- app/src/main/java/abhinav/hackdev/co/amortizedanalysis/MainActivity.java
+++ app/src/main/java/abhinav/hackdev/co/amortizedanalysis/MainActivity.java
@@ -2,14 +2,30 @@ package abhinav.hackdev.co.amortizedanalysis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
 import com.pixelcan.inkpageindicator.InkPageIndicator;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "TAG";
     private CustomAdapter customAdapter ;
     private CustomViewPager customViewPager ;
     private InkPageIndicator inkPageIndicator ;
+    private BarChart barChart ;
+
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +37,45 @@ public class MainActivity extends AppCompatActivity {
         customAdapter = new CustomAdapter(getFragmentManager()) ;
         customViewPager.setAdapter(customAdapter);
         inkPageIndicator.setViewPager(customViewPager);
+        barChart = (BarChart) findViewById(R.id.chart) ;
+        chartHandling() ;
+    }
+
+    private void chartHandling() {
+        barChart.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        barChart.setTouchEnabled(true);
+        barChart.setDrawGridBackground(false);
+        XAxis xAxis = barChart.getXAxis() ;
+        YAxis yAxis = barChart.getAxisLeft() ;
+        yAxis.setAxisLineColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        getData() ;
+
+        barChart.animateXY(2000, 2000) ;
+        barChart.invalidate();
+
+    }
+
+    public void getData() {
+        ArrayList<String> xVals = new ArrayList<>() ;
+        for (int i = 1; i < 9; i++) {
+            xVals.add("SEM " + i);
+        }
+
+        List<BarEntry> yVals = new ArrayList<>();
+
+        for(int j=1 ; j< 9 ; j++){
+            Log.d(TAG, "getData: " + j*2);
+            yVals.add(new BarEntry(j*2, j-1)) ;
+        }
+
+        BarDataSet barDataSet = new BarDataSet(yVals, "GPA") ;
+        BarData barData = new BarData(xVals, barDataSet) ;
+
+        barChart.setData(barData);
+    }
+
+    public void updateChart(){
+
     }
 }
